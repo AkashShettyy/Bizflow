abd50e567f39f32b6a8674047ac6cf42ed3320b9
AkashShettyy <akashshettyy22@gmail.com>
2026-09-13 16:57:44 +0530

    feat: add login endpoint

--- server/src/auth/controller.ts
+++ server/src/auth/controller.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
-import { registerUser } from "./service.js";
-import { registerSchema } from "./validation.js";
+import { loginUser,registerUser } from "./service.js";
+import {loginSchema, registerSchema } from "./validation.js";
 
 export const register = async (
   req: Request,
@@ -13,6 +13,20 @@ export const register = async (
   res.status(201).json({
     success: true,
     message: "Registration successful",
+    data: result,
+  });
+};
+
+export const login = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  const input = loginSchema.parse(req.body);
+  const result = await loginUser(input);
+
+  res.status(200).json({
+    success: true,
+    message: "Login successful",
     data: result,
   });
 };
